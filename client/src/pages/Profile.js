22117492658760a171d2e68159243824e0f96c0b
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-07-01 20:53:42 +0200

    fix left nav

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,11 +2,13 @@ import React, { useContext } from 'react';
 import Log from '../components/Log/Log';
 import UidContext from '../components/AppContext';
 import UpdateProfile from '../components/Profile/UpdateProfile';
+import LeftNav from '../components/LeftNav';
 
 const Profile = () => {
     const uid = useContext(UidContext);
     return (
         <div className="profil-page">
+            <LeftNav />
             {uid ? (
                 <UpdateProfile />
             ) : (
